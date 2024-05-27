a49dc715b0a51cb3e8b15264e53c986d0794dd2c
Serhii Kapitan <serejkakaapitan@gmail.com>
2024-05-27 23:59:38 +0300

    arrow

--- src/components/ProductList/Items.jsx
+++ src/components/ProductList/Items.jsx
@@ -1,101 +1,113 @@
-import { MobileOutlined, LaptopOutlined, TabletOutlined, ClockCircleOutlined, VideoCameraOutlined } from "@ant-design/icons";
+import {
+  MobileOutlined,
+  LaptopOutlined,
+  TabletOutlined,
+  ClockCircleOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
 import { HiDesktopComputer } from "react-icons/hi";
 import { PiTelevisionSimpleBold } from "react-icons/pi";
 import { FaHeadphonesSimple } from "react-icons/fa6";
 import { IoGameController } from "react-icons/io5";
 import { MdChildCare } from "react-icons/md";
 import { LuPercent } from "react-icons/lu";
-import BasketIcon from '../img/icons/basket-icon.svg'
+import BasketIcon from "../img/icons/basket-icon.svg";
 
 const items = [
-    {
-      key: '1',
-      label: (
-        <a target="_blank" rel="" href="" className="list-item">
-         <MobileOutlined />  <p>Smartphone</p>
-        </a>
-      ),
-    },
-    {
-      key: '2',
-      label: (
-        <a target="_blank" rel="" href="" className="list-item">
-         <LaptopOutlined />  Laptop
-        </a>
-      ),
-    },
-    {
-      key: '3',
-      label: (
-        <a target="_blank" rel="" href="" className="list-item">
-         <TabletOutlined /> Tablet
-        </a>
-      ),
-    },
-    {
-      key: '4',
-      label: (
-        <a target="_blank" rel="" href="" className="list-item">
-         <HiDesktopComputer /> PC
-        </a>
-      ),
-    },
-    {
-      key: '5',
-      label: (
-        <a target="_blank" rel="" href="" className="list-item">
-         <PiTelevisionSimpleBold /> TVs and Multimedia
-        </a>
-      ),
-    },
-    {
-      key: '6',
-      label: (
-        <a target="_blank" rel="" href="" className="list-item">
-         <ClockCircleOutlined/>  Smart-watch
-        </a>
-      ),
-    },
-    {
-      key: '7',
-      label: (
-        <a target="_blank" rel="" href="" className="list-item">
-         <FaHeadphonesSimple /> Audio
-        </a>
-      ),
-    },
-    {
-      key: '8',
-      label: (
-        <a target="_blank" rel="" href="" className="list-item">
-         <IoGameController /> Game console
-        </a>
-      ),
-    },
-    {
-      key: '9',
-      label: (
-        <a target="_blank" rel="" href="" className="list-item">
-         <VideoCameraOutlined />  Photo and video
-        </a>
-      ),
-    },
-    {
-      key: '10',
-      label: (
-        <a target="_blank" rel="" href="" className="list-item">
-         <MdChildCare /> KIDS
-        </a>
-      ),
-    },
-    {
-      key: '11',
-      label: (
-        <a target="_blank" rel="" href="" style={{color: 'red'}} className="list-item">
-         <LuPercent /> SALE
-        </a>
-      ),
-    },
+  {
+    key: "1",
+    label: (
+      <a target="_blank" rel="" href="" className="list-item">
+        <MobileOutlined /> <p>Smartphone</p>
+      </a>
+    ),
+  },
+  {
+    key: "2",
+    label: (
+      <a target="_blank" rel="" href="" className="list-item">
+        <LaptopOutlined /> Laptop
+      </a>
+    ),
+  },
+  {
+    key: "3",
+    label: (
+      <a target="_blank" rel="" href="" className="list-item">
+        <TabletOutlined /> Tablet
+      </a>
+    ),
+  },
+  {
+    key: "4",
+    label: (
+      <a target="_blank" rel="" href="" className="list-item">
+        <HiDesktopComputer /> PC
+      </a>
+    ),
+  },
+  {
+    key: "5",
+    label: (
+      <a target="_blank" rel="" href="" className="list-item">
+        <PiTelevisionSimpleBold /> TVs and Multimedia
+      </a>
+    ),
+  },
+  {
+    key: "6",
+    label: (
+      <a target="_blank" rel="" href="" className="list-item">
+        <ClockCircleOutlined /> Smart-watch
+      </a>
+    ),
+  },
+  {
+    key: "7",
+    label: (
+      <a target="_blank" rel="" href="" className="list-item">
+        <FaHeadphonesSimple /> Audio
+      </a>
+    ),
+  },
+  {
+    key: "8",
+    label: (
+      <a target="_blank" rel="" href="" className="list-item">
+        <IoGameController /> Game console
+      </a>
+    ),
+  },
+  {
+    key: "9",
+    label: (
+      <a target="_blank" rel="" href="" className="list-item">
+        <VideoCameraOutlined /> Photo and video
+      </a>
+    ),
+  },
+  {
+    key: "10",
+    label: (
+      <a target="_blank" rel="" href="" className="list-item">
+        <MdChildCare /> KIDS
+      </a>
+    ),
+  },
+  {
+    key: "11",
+    label: (
+      <a
+        target="_blank"
+        rel=""
+        href=""
+        style={{ color: "red" }}
+        className="list-item"
+      >
+        <LuPercent /> SALE
+      </a>
+    ),
+  },
 ];
 
 export default items;
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,200 +1,186 @@
-import React from 'react'
-import { Dropdown, Space, Divider } from 'antd';
-import items from './Items';
-import Right from '../img/icons/right.png'
-import Smartphone from '../img/catalog-icons/smartphone.svg'
-import laptop from '../img/catalog-icons/laptop.svg'
-import tablet from '../img/catalog-icons/tablet.svg'
-import PC from '../img/catalog-icons/pc.svg'
-import TV from '../img/catalog-icons/tv.svg'
-import watch from '../img/catalog-icons/watch.svg'
-import audio from '../img/catalog-icons/audio.svg'
-import console from '../img/catalog-icons/console.svg'
-import photo from '../img/catalog-icons/photo.svg'
-import kids from '../img/catalog-icons/kids.svg'
-import sale from '../img/catalog-icons/sale.svg'
-
+import React from "react";
+import { Dropdown, Space, Divider } from "antd";
+import items from "./Items";
+import Right from "../img/icons/right.png";
+import Smartphone from "../img/catalog-icons/smartphone.svg";
+import laptop from "../img/catalog-icons/laptop.svg";
+import tablet from "../img/catalog-icons/tablet.svg";
+import PC from "../img/catalog-icons/pc.svg";
+import TV from "../img/catalog-icons/tv.svg";
+import watch from "../img/catalog-icons/watch.svg";
+import audio from "../img/catalog-icons/audio.svg";
+import console from "../img/catalog-icons/console.svg";
+import photo from "../img/catalog-icons/photo.svg";
+import kids from "../img/catalog-icons/kids.svg";
+import sale from "../img/catalog-icons/sale.svg";
 
 export default function ProductList() {
-    return (
-      <div className='product-list'>
-        <Dropdown
-          menu={items}
-          trigger={['click']}
-          placement='right'
-        >
-          <a href='' className='catalog-nav' onClick={(e) => e.preventDefault()}>
-            <Space className='catalog-item' style={{ alignItems: 'center'}}>
-              <div className='catalog-list'>
-                <img src={Smartphone} alt="Smartphone" /> Smartphone
-              </div>
-              <img src={Right} alt="Right Arrow" style={{ marginLeft: 'auto' }} />
-            </Space>
-          </a>
-          
-        </Dropdown>
-        <Divider style={{marginTop: '8px', color:'#808080'}}/>
-  
-        <Dropdown
-          menu={items}
-          trigger={['click']}
-          placement='right'
-        >
-          <a href='' className='catalog-nav' onClick={(e) => e.preventDefault()}>
-            <Space className='catalog-item' style={{ alignItems: 'center', gap: '190px'}}>
-              <div className='catalog-list'>
-                <img src={laptop} alt="Laptop" /> Laptop
-              </div>
-              <img src={Right} alt="Right Arrow" style={{ marginLeft: 'auto' }} />
-            </Space>
-          </a>
-        </Dropdown>
-        <Divider style={{marginTop: '8px', color:'#808080'}}/>
+  return (
+    <div className="product-list">
+      <Dropdown menu={items} trigger={["click"]} placement="right">
+        <a href="" className="catalog-nav" onClick={(e) => e.preventDefault()}>
+          <Space className="catalog-item" style={{ alignItems: "center" }}>
+            <div className="catalog-list">
+              <img src={Smartphone} alt="Smartphone" /> Smartphone
+            </div>
+            <i className="arrow right"></i>
+          </Space>
+        </a>
+      </Dropdown>
+      <Divider style={{ marginTop: "8px", color: "#808080" }} />
 
-        <Dropdown
-          menu={items}
-          trigger={['click']}
-          placement='right'
-        >
-          <a href='' className='catalog-nav' onClick={(e) => e.preventDefault()}>
-            <Space className='catalog-item' style={{ alignItems: 'center', gap: '200px' }}>
-              <div className='catalog-list'>
-                <img src={tablet} alt="Laptop" /> Tablet
-              </div>
-              <img src={Right} alt="Right Arrow" style={{ marginLeft: 'auto' }} />
-            </Space>
-          </a>
-        </Dropdown>
-        <Divider style={{marginTop: '8px', color:'#808080'}}/>
+      <Dropdown menu={items} trigger={["click"]} placement="right">
+        <a href="" className="catalog-nav" onClick={(e) => e.preventDefault()}>
+          <Space
+            className="catalog-item"
+            style={{ alignItems: "center", gap: "190px" }}
+          >
+            <div className="catalog-list">
+              <img src={laptop} alt="Laptop" /> Laptop
+            </div>
+            <i className="arrow right"></i>
+          </Space>
+        </a>
+      </Dropdown>
+      <Divider style={{ marginTop: "8px", color: "#808080" }} />
 
-        <Dropdown
-          menu={items}
-          trigger={['click']}
-          placement='right'
-        >
-          <a href='' className='catalog-nav' onClick={(e) => e.preventDefault()}>
-            <Space className='catalog-item' style={{ alignItems: 'center', gap: '235px'}}>
-              <div className='catalog-list'>
-                <img src={PC} alt="Laptop" /> PC
-              </div>
-              <img src={Right} alt="Right Arrow" style={{ marginLeft: 'auto' }} />
-            </Space>
-          </a>
-        </Dropdown>
-        <Divider style={{marginTop: '8px', color:'#808080'}}/>
+      <Dropdown menu={items} trigger={["click"]} placement="right">
+        <a href="" className="catalog-nav" onClick={(e) => e.preventDefault()}>
+          <Space
+            className="catalog-item"
+            style={{ alignItems: "center", gap: "200px" }}
+          >
+            <div className="catalog-list">
+              <img src={tablet} alt="Laptop" /> Tablet
+            </div>
+            <i className="arrow right"></i>
+          </Space>
+        </a>
+      </Dropdown>
+      <Divider style={{ marginTop: "8px", color: "#808080" }} />
 
-        <Dropdown
-          menu={items}
-          trigger={['click']}
-          placement='right'
-        >
-          <a href='' className='catalog-nav' onClick={(e) => e.preventDefault()}>
-            <Space className='catalog-item' style={{ alignItems: 'center', gap: '50px' }}>
-              <div className='catalog-list'>
-                <img src={TV} alt="Laptop" /> 
-                TVs and Multimedia
-              </div>
-              <img src={Right} alt="Right Arrow" style={{ marginLeft: 'auto' }} />
-            </Space>
-          </a>
-        </Dropdown>
-        <Divider style={{marginTop: '8px', color:'#808080'}}/>
+      <Dropdown menu={items} trigger={["click"]} placement="right">
+        <a href="" className="catalog-nav" onClick={(e) => e.preventDefault()}>
+          <Space
+            className="catalog-item"
+            style={{ alignItems: "center", gap: "235px" }}
+          >
+            <div className="catalog-list">
+              <img src={PC} alt="Laptop" /> PC
+            </div>
+            <i className="arrow right"></i>
+          </Space>
+        </a>
+      </Dropdown>
+      <Divider style={{ marginTop: "8px", color: "#808080" }} />
 
-        <Dropdown
-          menu={items}
-          trigger={['click']}
-          placement='right'
-        >
-          <a href='' className='catalog-nav' onClick={(e) => e.preventDefault()}>
-            <Space className='catalog-item' style={{ alignItems: 'center', gap: '126px'}}>
-              <div className='catalog-list'>
-                <img src={watch} alt="Laptop" /> Smart-watch
-              </div>
-              <img src={Right} alt="Right Arrow" style={{ marginLeft: 'auto' }} />
-            </Space>
-          </a>
-        </Dropdown>
-        <Divider style={{marginTop: '8px', color:'#808080'}}/>
+      <Dropdown menu={items} trigger={["click"]} placement="right">
+        <a href="" className="catalog-nav" onClick={(e) => e.preventDefault()}>
+          <Space
+            className="catalog-item"
+            style={{ alignItems: "center", gap: "50px" }}
+          >
+            <div className="catalog-list">
+              <img src={TV} alt="Laptop" />
+              TVs and Multimedia
+            </div>
+            <i className="arrow right"></i>
+          </Space>
+        </a>
+      </Dropdown>
+      <Divider style={{ marginTop: "8px", color: "#808080" }} />
 
-        <Dropdown
-          menu={items}
-          trigger={['click']}
-          placement='right'
-        >
-          <a href='' className='catalog-nav' onClick={(e) => e.preventDefault()}>
-            <Space className='catalog-item' style={{ alignItems: 'center', gap: '200px' }}>
-              <div className='catalog-list'>
-                <img src={audio} alt="Laptop" /> Audio
-              </div>
-              <img src={Right} alt="Right Arrow" style={{ marginLeft: 'auto' }} />
-            </Space>
-          </a>
-        </Dropdown>
-        <Divider style={{marginTop: '8px', color:'#808080'}}/>
+      <Dropdown menu={items} trigger={["click"]} placement="right">
+        <a href="" className="catalog-nav" onClick={(e) => e.preventDefault()}>
+          <Space
+            className="catalog-item"
+            style={{ alignItems: "center", gap: "126px" }}
+          >
+            <div className="catalog-list">
+              <img src={watch} alt="Laptop" /> Smart-watch
+            </div>
+            <i className="arrow right"></i>
+          </Space>
+        </a>
+      </Dropdown>
+      <Divider style={{ marginTop: "8px", color: "#808080" }} />
 
-        <Dropdown
-          menu={items}
-          trigger={['click']}
-          placement='right'
-        >
-          <a href='' className='catalog-nav' onClick={(e) => e.preventDefault()}>
-            <Space className='catalog-item' style={{ alignItems: 'center', gap: '110px'}}>
-              <div className='catalog-list'>
-                <img src={console} alt="Laptop" /> Game console
-              </div>
-              <img src={Right} alt="Right Arrow" style={{ marginLeft: 'auto' }} />
-            </Space>
-          </a>
-        </Dropdown>
-        <Divider style={{marginTop: '8px', color:'#808080'}}/>
+      <Dropdown menu={items} trigger={["click"]} placement="right">
+        <a href="" className="catalog-nav" onClick={(e) => e.preventDefault()}>
+          <Space
+            className="catalog-item"
+            style={{ alignItems: "center", gap: "200px" }}
+          >
+            <div className="catalog-list">
+              <img src={audio} alt="Laptop" /> Audio
+            </div>
+            <i className="arrow right"></i>
+          </Space>
+        </a>
+      </Dropdown>
+      <Divider style={{ marginTop: "8px", color: "#808080" }} />
 
-        <Dropdown
-          menu={items}
-          trigger={['click']}
-          placement='right'
-        >
-          <a href='' className='catalog-nav' onClick={(e) => e.preventDefault()}>
-            <Space className='catalog-item' style={{ alignItems: 'center', gap: '86px'}}>
-              <div className='catalog-list'>
-                <img src={photo} alt="Laptop" /> Photo and video
-              </div>
-              <img src={Right} alt="Right Arrow" style={{ marginLeft: 'auto' }} />
-            </Space>
-          </a>
-        </Dropdown>
-        <Divider style={{marginTop: '8px', color:'#808080'}}/>
+      <Dropdown menu={items} trigger={["click"]} placement="right">
+        <a href="" className="catalog-nav" onClick={(e) => e.preventDefault()}>
+          <Space
+            className="catalog-item"
+            style={{ alignItems: "center", gap: "110px" }}
+          >
+            <div className="catalog-list">
+              <img src={console} alt="Laptop" /> Game console
+            </div>
+            <i className="arrow right"></i>
+          </Space>
+        </a>
+      </Dropdown>
+      <Divider style={{ marginTop: "8px", color: "#808080" }} />
 
-        <Dropdown
-          menu={items}
-          trigger={['click']}
-          placement='right'
-        >
-          <a href='' className='catalog-nav' onClick={(e) => e.preventDefault()}>
-            <Space className='catalog-item' style={{ alignItems: 'center', gap: '210px'}}>
-              <div className='catalog-list'>
-                <img src={kids} alt="Laptop" /> KIDS
-              </div>
-              <img src={Right} alt="Right Arrow" style={{ marginLeft: 'auto' }} />
-            </Space>
-          </a>
-        </Dropdown>
-        <Divider style={{marginTop: '8px', color:'#808080'}}/>
+      <Dropdown menu={items} trigger={["click"]} placement="right">
+        <a href="" className="catalog-nav" onClick={(e) => e.preventDefault()}>
+          <Space
+            className="catalog-item"
+            style={{ alignItems: "center", gap: "86px" }}
+          >
+            <div className="catalog-list">
+              <img src={photo} alt="Laptop" /> Photo and video
+            </div>
+            <i className="arrow right"></i>
+          </Space>
+        </a>
+      </Dropdown>
+      <Divider style={{ marginTop: "8px", color: "#808080" }} />
 
-        <Dropdown
-          menu={items}
-          trigger={['click']}
-          placement='right'
-        >
-          <a href='' className='catalog-nav' onClick={(e) => e.preventDefault()}>
-            <Space className='catalog-item' style={{ alignItems: 'center', marginBottom: '0px', gap: '210px' }}>
-              <div className='catalog-list' style={{color:'red'}}>
-                <img src={sale} alt="Laptop" /> SALE
-              </div>
-              <img src={Right} alt="Right Arrow" style={{ marginLeft: 'auto' }} />
-            </Space>
-          </a>
-        </Dropdown>
-        <Divider style={{marginTop: '8px', marginBottom: '0px', color:'#808080'}}/>
-      </div>
-    );
-  }
+      <Dropdown menu={items} trigger={["click"]} placement="right">
+        <a href="" className="catalog-nav" onClick={(e) => e.preventDefault()}>
+          <Space
+            className="catalog-item"
+            style={{ alignItems: "center", gap: "210px" }}
+          >
+            <div className="catalog-list">
+              <img src={kids} alt="Laptop" /> KIDS
+            </div>
+            <i className="arrow right"></i>
+          </Space>
+        </a>
+      </Dropdown>
+      <Divider style={{ marginTop: "8px", color: "#808080" }} />
+
+      <Dropdown menu={items} trigger={["click"]} placement="right">
+        <a href="" className="catalog-nav" onClick={(e) => e.preventDefault()}>
+          <Space
+            className="catalog-item"
+            style={{ alignItems: "center", marginBottom: "0px", gap: "210px" }}
+          >
+            <div className="catalog-list" style={{ color: "red" }}>
+              <img src={sale} alt="Laptop" /> SALE
+            </div>
+            <i className="arrow right"></i>
+          </Space>
+        </a>
+      </Dropdown>
+      <Divider
+        style={{ marginTop: "8px", marginBottom: "0px", color: "#808080" }}
+      />
+    </div>
+  );
+}
