3afba939f9c8bde8dc07021f159f731db261d954
Serhii Kapitan <serejkakaapitan@gmail.com>
2024-05-28 00:20:45 +0300

    logo fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import Logo from '../img/gadget-house-logo.png'
+import React from "react";
+import Logo from "../img/gadget-house-logo.png";
 
 export default function Header() {
   return (
-    <header className='header'>
-      <img src={Logo}/>
+    <header className="header">
+      <a href="#">
+        <p className="nunito-sans-logo">GadgetHouse</p>
+      </a>
     </header>
-  )
+  );
 }
