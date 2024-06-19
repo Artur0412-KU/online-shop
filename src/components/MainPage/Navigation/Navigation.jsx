2f0545884ff7718502232cbb0145b5f02620e59f
Artur <kudyrkoartur@gmail.com>
2024-06-19 11:09:43 +0300

    modified:   src/components/MainPage/Navigation/Navigation.jsx 	new file:   src/components/MainPage/Navigation/constants.js 	new file:   src/components/MainPage/img/icons/icons.js

--- src/components/MainPage/Navigation/Navigation.jsx
+++ src/components/MainPage/Navigation/Navigation.jsx
@@ -2,37 +2,9 @@ import React from 'react';
 import Catalog from '../Catalog/Catalog';
 import Search from '../Search/Search';
 import ButtonIcon from './Button/ButtonIcon';
-
-const DATA = [
-  {
-    key: '1',
-    img: '/src/components/MainPage/img/icons/call-icon.svg',
-    hoverImg: '/src/components/MainPage/img/icons/phone-icon-white.svg',
-    clickImg: '/src/components/MainPage/img/icons/phone-icon-white.svg',
-  },
-  {
-    key: '2',
-    img: '/src/components/MainPage/img/icons/heart-icon.svg',
-    hoverImg: '/src/components/MainPage/img/icons/heart-icon-white.svg',
-    clickImg: '/src/components/MainPage/img/icons/heart-icon-white.svg',
-  },
-  {
-    key: '3',
-    img: '/src/components/MainPage/img/icons/user-icon.svg',
-    hoverImg: '/src/components/MainPage/img/icons/user-icon-white.svg',
-    clickImg: '/src/components/MainPage/img/icons/user-icon-white.svg',
-  },
-  {
-    key: '4',
-    img: '/src/components/MainPage/img/icons/basket-icon.svg',
-    hoverImg: '/src/components/MainPage/img/icons/basket-icon-white.svg',
-    clickImg: '/src/components/MainPage/img/icons/basket-icon-white.svg',
-  },
-];
+import DATA from './constants';
 
 export default function Navigation() {
-  // const [data, setData] = useState(DATA);
-
   return (
     <nav className="navigation">
       <Catalog />
