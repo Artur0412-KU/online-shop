3d77bac32756f0400225b7d999215c5b7c410aaf
Artur <71281020+Artur0412-KU@users.noreply.github.com>
2024-08-01 16:45:56 +0300

    modified:   src/components/MainPage/Brands/Card/ProductCard.jsx 	modified:   src/index.css

--- src/components/MainPage/Brands/Card/ProductCard.jsx
+++ src/components/MainPage/Brands/Card/ProductCard.jsx
@@ -3,15 +3,13 @@ import { Card } from 'antd';
 import DATA_CARD from './constants';
 
 export default function BrandCard() {
-  // const [data, setData] = useState(DATA_CARD);
-
   return (
-    <>
+    <div className='card-container card-container-brands '>
       {DATA_CARD.map((item) => (
         <Card key={item.key} className="brand-card">
           <img className="card-img" src={item.img} alt="#" />
         </Card>
       ))}
-    </>
+    </div >
   );
 }
